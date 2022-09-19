fe37a07bbaffb596327bb8e800800233e3ddf05c
Marcos <marvim_sl@hotmail.com>
2022-09-19 02:01:43 -0300

    first commit

---
+++ src/test/java/com/onsafety/demoproj/DemoprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onsafety.demoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
